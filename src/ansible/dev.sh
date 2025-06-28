5b34154ed420f5b554e0b0e00eb3cc7d189931ec
DarkStar <darkstar@unisocial.net>
2025-06-29 02:18:20 +0800

    0

--- src/ansible/dev.sh
+++ src/ansible/dev.sh
@@ -4,3 +4,4 @@ echo "*/" > roles/dev/.gitignore
 cd roles/dev
 git clone git@github.com:Unicorn-OS/ansible-role-Gnome-Remote-Desktop.git
 git clone git@github.com:Unicorn-OS/ansible-role-Gnome-Remote-Desktop-Connection.git
+git clone git@github.com:Unicorn-OS/ansible-role-Remmina-RDP.git
